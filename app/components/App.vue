8c3d79e67c93ba42660846d3ae5c7376232bb1a4
Kevin Davis <kevincarrolldavis@gmail.com>
2020-02-09 00:38:16 -0500

    Begin GUI flow of app

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,25 +1,42 @@
 <template>
-    <Page>
-        <ActionBar title="Welcome to NativeScript-Vue!"/>
-        <GridLayout columns="*" rows="*">
-            <Label class="message" :text="msg" col="0" row="0"/>
-        </GridLayout>
+    <Page @loaded="checkDevices">
+        <ActionBar title="ecoglink BMI"/>
+        <StackLayout>
+            <Label class="message" :text="inputDeviceText" col="0" row="0"/>
+            <Label class="message" :text="outputDeviceText" col="0" row="0"/>
+        </StackLayout>
     </Page>
 </template>
 
 <script lang="ts">
-  export default {
-    data() {
-      return {
-        msg: 'Hello World?'
-      }
-    }
-  }
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { EventData  } from 'tns-core-modules/data/observable';
+
+@Component
+export default class App extends Vue {
+
+	inputDevice: string = '';
+	outputDevice: string = '';
+	
+	// Computed Properties
+	get inputDeviceText(): string {
+		return `Input Device: ${this.inputDevice}`;
+	}
+
+	get outputDeviceText(): string {
+		return `Output Device: ${this.outputDevice}`;
+	}
+
+	// Methods
+	checkDevices(args: EventData) {
+		console.log("Page Loaded");
+	}
+};
 </script>
 
 <style scoped>
     ActionBar {
-        background-color: #53ba82;
+        background-color: #002c15;
         color: #ffffff;
     }
 
@@ -27,6 +44,5 @@
         vertical-align: center;
         text-align: center;
         font-size: 20;
-        color: #333333;
     }
 </style>
