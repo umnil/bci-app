3c2d964e77eafec702b4d9238de9a1359f31d6a5
Kevin Davis <kevincarrolldavis@gmail.com>
2020-02-14 18:56:55 -0500

    Added the side drawer and a communication bus

--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,17 @@
 import Vue from 'nativescript-vue'
 import App from './components/App'
 
+import { Fontawesome } from 'nativescript-fontawesome';
+Fontawesome.init();
 
-
+Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer);
 
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
+Vue.prototype.$bus = new Vue();
 
 new Vue({
-  
   render: h => h('frame', [h(App)])
 }).$start()
