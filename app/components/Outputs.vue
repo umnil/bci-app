0132f9099ee51f42033e23ad76f5f00fbd6c452b
Kevin Davis <kevincarrolldavis@gmail.com>
2020-03-11 18:11:55 -0400

    update

--- app/components/Outputs.vue
+++ app/components/Outputs.vue
@@ -6,7 +6,7 @@
 				<v-template>
 					<StackLayout orientation="horizontal" width=100%>
 						<Label :class="selectionclass(device.device_name)" :text="marker"></Label>
-						<Label class="device-listing" :text="device.device_name" @tap="select(device.device_name)" />
+						<Label class="device-listing" :text="device.device_name" @tap="select(device)" />
 						<Label class="fa settings" :text="settings_symbol" @tap="toSettings(device)"/>
 					</StackLayout>
 				</v-template>
@@ -18,34 +18,47 @@
 <script lang="ts">
 import { Vue, Component, Watch } from 'vue-property-decorator';
 import connectionDelegate from '../utils/ConnectionDelegate';
+import DeviceSettings from './DeviceSettings';
 
 @Component
 export default class Outputs extends Vue {
 
-
 	// Data
 	cd: any = connectionDelegate;
+	bus: any = (this as any).$bus;
 	marker: string = String.fromCharCode(0xf00c);
 	settings_symbol: string = String.fromCharCode(0xf013);
+	selected_device_setting: string = "";
 	devices: any[] = [];
 
 	// Methods
 	constructor() {
 		super();
-		(this as any).$bus.Outputs = this;
+		this.bus.Outputs = this;
 	}
 
-	loadDevices() {
-		this.getOutputDevices();
-	}
+	select(device: any): void {
+		console.log(`Selected: ${device.device_name}`);
+		this.selected_device = device.device_name;
+		this.setOutputSettings();
+		(this.$refs.deviceList as any).refresh();
 
-	select(name: string): void {
-		this.selected_device = name;
-		console.log(this.selected_device);
 	}
 
 	toSettings(deviceName: string): void {
 
+	}
+
+	loadDevices(): void {
+		this.getOutputDevices();
+	}
+
+	setOutputSettings(): void {
+		let outputSettings: any = {
+			'selected_device': this.selected_device,
+			'devices': this.devices
+		}
+		this.cd.setOutputSettings(outputSettings);
 	}
 
 	get selected_device(): string {
