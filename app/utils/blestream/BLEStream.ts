f2f7370b556753081d68319c325276e9c772c7f7
Kevin Davis <kevincarrolldavis@gmail.com>
2020-03-13 00:54:41 -0400

    Finished readstream commands, starting on writestream

--- app/utils/blestream/BLEStream.ts
+++ app/utils/blestream/BLEStream.ts
@@ -1,5 +1,6 @@
 import sleep from './Sleep';
-import { RequestQueue } from './RequestQueue';
+import DataUtil from './DataUtil';
+import { RequestQueue, ReadRequest, WriteRequest } from './RequestQueue';
 import { 
 	Bluetooth,
 	ReadOptions,
@@ -7,20 +8,27 @@ import {
 	ReadResult
 } from 'nativescript-bluetooth';
 
-export default class BLEStream extends Bluetooth {
+export interface Transmission {
+	msg: string;
+	payload: any;
+}
 
+export class BLEStream extends Bluetooth {
+
+	private dataUtil: DataUtil = new DataUtil();
 	private requestQueue: RequestQueue = new RequestQueue();
 
 	// Methods
 	async streamRead(readOptions: ReadOptions): Promise<ReadResult> {
 
-		let requestID: string = this.requestQueue.addReadRequest(readOptions);
+		let requestID: string = this.requestQueue.addRequest(readOptions);
 
-		while(!this.requestQueue.checkReadQueue(requestID)) {
+		while(!this.requestQueue.checkQueue(requestID)) {
 			await sleep(10);
 		}
 
 		let result: ReadResult = await this.executeStreamRead(requestID);
+		this.requestQueue.popReadQueue();
 		return result;
 	}
 
@@ -28,16 +36,70 @@ export default class BLEStream extends Bluetooth {
 
 	}
 
-	async executeStreamRead(id: string): Promise<ReadResult> {
-		// Change this
+	private async executeStreamRead(id: string): Promise<ReadResult> {
+		
+		let readRequest: ReadRequest = <ReadRequest>this.requestQueue.getRequest(id);
+		let readResult: ReadResult = await this.read(<ReadOptions>readRequest);
+
+		if(readRequest.transmitting) this.recvData(readRequest, readResult);
+		else this.recvSize(readRequest, readResult);
+
+		// We're finished with this request, pop it off the stack
+		this.requestQueue.popReadQueue;
 		return {
-			serviceUUID: '',
-			characteristicUUID: '',
-			value: new ArrayBuffer(null)
+			serviceUUID: readRequest.serviceUUID,
+			characteristicUUID: readRequest.characteristicUUID,
+			value: readRequest.result
 		};
 	}
 
-	async executeStreamWrite(id: string): Promise<void> {
+	private async executeStreamWrite(id: string): Promise<void> {
 
 	}
+
+	// processRead
+	//
+	// processWrite
+	//
+	// recvSize
+	private async recvSize(readRequest: ReadRequest, result: ReadResult): Promise<void> {
+
+		readRequest.transmitting = true;
+		(readRequest as any).pointer = 0;
+
+		let transmission: Transmission = this.dataUtil.data2object(result.value);
+		let msg: string = transmission['msg'];
+		
+		if(msg != 'SIZE') return;
+
+		let size: number = <number>transmission['payload'];
+		readRequest.value = new ArrayBuffer(size);
+		await this.executeStreamRead(readRequest.id);
+	}
+	//
+	// sendSize
+	//
+	// recvData
+	private async recvData(readRequest: ReadRequest, result: ReadResult): Promise<void> {
+		let transmission: Transmission = this.dataUtil.data2object(result.value);
+		let msg: string = transmission['msg'];
+
+		if(msg != 'DATA') return;
+
+		let dataString: string = <string>transmission['payload'];
+		let data: ArrayBuffer = this.dataUtil.str2arraybuffer(dataString);
+		this.dataUtil.appendData(readRequest, data);
+
+		let remainingBytes: number = readRequest.result.byteLength - (readRequest as any).pointer;
+
+		if(remainingBytes > 0) {
+			await this.executeStreamRead(readRequest.id);
+			return;
+		}
+
+		readRequest.transmitting = false;
+		(readRequest as any).pointer = 0;
+	}
+	//
+	// sendData
 }
