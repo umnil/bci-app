c0ad0dea71a1df35061d374b8db4b4a26480b717
Kevin Davis <kevincarrolldavis@gmail.com>
2020-03-31 17:19:41 -0400

    Set chunk size to 400 and ensured write recursive func works

--- app/utils/blestream/BLEStream.ts
+++ app/utils/blestream/BLEStream.ts
@@ -20,7 +20,7 @@ export class BLEStream extends Bluetooth {
 	private chunkSize: number;
 
 	// Methods
-	constructor(chunkSize=500) {
+	constructor(chunkSize=400) {
 		super();
 		this.chunkSize = chunkSize;
 	}
@@ -43,8 +43,9 @@ export class BLEStream extends Bluetooth {
 	async streamWrite(writeOptions: WriteOptions): Promise<any> {
 
 		let requestID: string = this.requestQueue.addRequest(writeOptions);
+		this.log(`requestID: ${requestID}`);
 
-		while(this.requestQueue.checkQueue(requestID)) {
+		while(!this.requestQueue.checkQueue(requestID)) {
 			await sleep(10);
 		}
 
@@ -72,10 +73,13 @@ export class BLEStream extends Bluetooth {
 	}
 
 	private async executeStreamWrite(id: string): Promise<void> {
+		this.log(`WRITE! ${id}`);
 		let writeRequest: WriteRequest = <WriteRequest>this.requestQueue.getRequest(id);
+		this.log(`request: ${writeRequest.id}`)
+		this.log(`Transmission state: ${writeRequest.transmitting}`);
 
-		if(writeRequest.transmitting) this.sendData(writeRequest);
-		else this.sendSize(writeRequest);
+		if(writeRequest.transmitting) await this.sendData(writeRequest);
+		else await this.sendSize(writeRequest);
 	}
 
 	// processRead
@@ -101,12 +105,14 @@ export class BLEStream extends Bluetooth {
 	//
 	// sendSize
 	private async sendSize(writeRequest: WriteRequest): Promise<void> {
+		this.log(`Send Size`);
 
 		writeRequest.transmitting = true;
 		(writeRequest as any).pointer = 0;
 
 		let strData: string = this.dataUtil.hex2str(writeRequest.value);
 		let size: number = strData.length;
+		this.log(`data size: ${size}`);
 		writeRequest.result = this.dataUtil.str2arraybuffer(strData);
 
 		let transmission: Transmission = {
@@ -114,10 +120,12 @@ export class BLEStream extends Bluetooth {
 			'payload': size
 		};
 
-		let writeValue: string = this.dataUtil.value2hex(transmission);
+		//let writeValue: string = this.dataUtil.value2hex(transmission);
+		let writeValue: string = JSON.stringify(transmission);
 		let tempWriteOptions: WriteOptions = <WriteOptions>JSON.parse(JSON.stringify(writeRequest));
 		tempWriteOptions.value = writeValue;
-		await this.write(tempWriteOptions);
+		this.log(`WriteOptions: ${JSON.stringify(tempWriteOptions)}`);
+		await this.write(tempWriteOptions).then(_ => this.log('success'), err => this.log(`err: ${err}`));
 		await this.executeStreamWrite(writeRequest.id);
 	}
 	//
@@ -154,6 +162,7 @@ export class BLEStream extends Bluetooth {
 		let dataSize: number = remainingBytes > this.chunkSize ? this.chunkSize : remainingBytes;
 		let curChunk: string = this.dataUtil.arraybuffer2str(writeRequest.result).substr(pointer, dataSize);
 		pointer += dataSize;
+		(writeRequest as any).pointer = pointer;
 
 		let transmission: Transmission = {
 			'msg': 'DATA',
@@ -161,10 +170,17 @@ export class BLEStream extends Bluetooth {
 		};
 		remainingBytes = writeRequest.result.byteLength - pointer;
 
-		let writeValue: string = this.dataUtil.value2hex(transmission);
+		let writeValue: string = JSON.stringify(transmission);
 		let tempWriteOptions: WriteOptions = <WriteOptions>JSON.parse(JSON.stringify(writeRequest));
 		tempWriteOptions.value = writeValue;
 		await this.write(tempWriteOptions);
-		await this.executeStreamWrite(writeRequest.id);
+
+		if(remainingBytes > 0) {
+			await this.executeStreamWrite(writeRequest.id);
+			return;
+		}
+
+		writeRequest.transmitting = false;
+		(writeRequest as any).pointer = 0;
 	}
 }
