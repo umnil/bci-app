b46476e33d650dd5ad32f02fa8c41d14ddd5d92f
Kevin Davis <kevincarrolldavis@gmail.com>
2020-08-06 23:24:32 -0400

    Register the new components

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,19 @@
 import Vue from 'nativescript-vue';
 import App from './components/App';
 import StatusIndicator from './components/StatusIndicator';
-import DeviceSettingItem from './components/DeviceSettingItem';
+import DeviceSettingButton from './components/DeviceSettings/Button';
+import DeviceSettingSlider from './components/DeviceSettings/Slider';
+import DeviceSettingListPicker from './components/DeviceSettings/ListPicker';
 
 import { Fontawesome } from 'nativescript-fontawesome';
 Fontawesome.init();
 
 Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer);
 Vue.component('StatusIndicator', StatusIndicator);
-Vue.component('DeviceSettingItem', DeviceSettingItem);
+
+Vue.component('DeviceSettingButton', DeviceSettingButton);
+Vue.component('DeviceSettingSlider', DeviceSettingSlider);
+Vue.component('DeviceSettingListPicker', DeviceSettingListPicker);
 
 
 // Prints Vue logs when --env.production is *NOT* set while building
