83ef38e0048ff386e38e53ed6b20e8d53248a4f7
Kevin Davis <kevincarrolldavis@gmail.com>
2021-04-19 14:12:28 -0400

    Adding the chart plugin

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'nativescript-vue';
 import App from './components/App';
 import RadSideDrawer from 'nativescript-ui-sidedrawer/vue';
+import RadChart from 'nativescript-ui-chart/vue';
 import StatusIndicator from './components/StatusIndicator';
 import DeviceSettingButton from './components/DeviceSettings/Button';
 import DeviceSettingSlider from './components/DeviceSettings/Slider';
@@ -11,6 +12,7 @@ Fontawesome.init();
 
 //Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer);
 Vue.use(RadSideDrawer);
+Vue.use(RadChart);
 Vue.component('StatusIndicator', StatusIndicator);
 
 Vue.component('DeviceSettingButton', DeviceSettingButton);
