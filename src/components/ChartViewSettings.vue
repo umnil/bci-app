57afe9fa99c1bab1f7e41e861a69496ddfa3ca3e
Kevin Davis <kevincarrolldavis@gmail.com>
2021-05-21 14:33:50 -0400

    New Chart Settings view

---
+++ src/components/ChartViewSettings.vue
@@ -0,0 +1,20 @@
+<template>
+	<Page>
+		<ActionBar text="Chart View Settings" />
+		<StackLayout>
+			<Label text="Hi" />
+		</StackLayout>
+	</Page>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch } from 'vue-property-decorator';
+
+@Component
+export default class ChartViewSettings extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
